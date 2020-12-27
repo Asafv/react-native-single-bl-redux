acf704bcc7d4167c5365eb672b8ee91e835dddcd
domainio <omega@omegas-MacBook-Pro.local>
2020-12-27 14:58:26 +0200

    fine tune

--- src/components/MovieListItem.js
+++ src/components/MovieListItem.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Text, View, Image, Pressable } from 'react-native';
+import { Text, View, Image, Pressable, StyleSheet } from 'react-native';
 import { Icon } from 'react-native-elements'
 
 const MovieListItem = ({ item }, onPress) => {
-  
+
   const handleOnPress = (item) => {
     onPress(item);
   }
@@ -11,30 +11,30 @@ const MovieListItem = ({ item }, onPress) => {
   const { image, id, name, score } = item;
   return (
     <Pressable onPress={() => handleOnPress(item)}>
-      <View style={{ flexDirection: 'row', alignItems: 'center', paddingHorizontal: 20 }}>
+      <View style={styles.container}>
         <Image
-          style={{ width: 50, height: 50, borderRadius: 25 }}
+          style={styles.image}
           source={{ uri: image }}
         />
-        <View style={{ paddingLeft: 15, flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
-          <Text style={{
-            color: 'yellow',
-            fontSize: 24,
-            flex: 1,
-          }} ellipsizeMode='tail' numberOfLines={1}>{name}</Text>
+        <View style={styles.textContainer}>
+          <Text style={styles.name} ellipsizeMode='tail' numberOfLines={1}>{name}</Text>
           <View style={{ flexDirection: 'row' }}>
             <Icon
               name='star' color='#FFD700' type='font-awesome' />
-            <Text style={{
-              color: '#FFD700',
-              fontSize: 20,
-              paddingLeft: 5,
-            }}>{Math.round(score * 10) / 10}</Text>
+            <Text style={styles.rating}>{Math.round(score * 10) / 10}</Text>
           </View>
         </View>
       </View>
     </Pressable>
   )
 };
+
+const styles = StyleSheet.create({
+  container: { flexDirection: 'row', alignItems: 'center', paddingHorizontal: 20 },
+  image: { width: 50, height: 50, borderRadius: 25 },
+  textContainer: { paddingLeft: 15, flex: 1, flexDirection: 'row', justifyContent: 'space-between' },
+  name: { color: 'yellow', fontSize: 24, flex: 1 },
+  rating: { color: '#FFD700', fontSize: 20, paddingLeft: 5 }
+});
 
 export default MovieListItem;
