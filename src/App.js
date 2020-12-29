4997dce785b72e7e60c13db9bc04d2fa175d9209
domainio <omega@omegas-MacBook-Pro.local>
2020-12-29 16:05:40 +0200

    with local store

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-import { Provider } from 'react-redux';
+import {
+  Provider,
+  createStoreHook,
+  createDispatchHook,
+  createSelectorHook,
+} from 'react-redux';
+import { createStore } from 'redux';
 import { store } from 'redux-logic-layer';
 import Router from './router/Router';
 
+const AppContext = React.createContext(null);
+export const useAppStore = createStoreHook(AppContext);
+export const useAppDispatch = createDispatchHook(AppContext);
+export const useAppSelector = createSelectorHook(AppContext);
+
+const tempRducer = (state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'type1':
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    default: return state;
+  }
+};
+
+const appStore = createStore(tempRducer);
+
+// const appStore = createStore(rootReducer);
+//<Provider context={AppContext} store={appStore}></Provider>
 const App: () => React$Node = () => {
   return (
     <Provider store={store}>
-      <StatusBar barStyle="dark-content" />
-      <Router />
+      <Provider context={AppContext} store={appStore}>
+        <StatusBar barStyle="dark-content" />
+        <Router />
+      </Provider>
     </Provider>
   );
 };
--- src/screens/MovieListScreen.js
+++ src/screens/MovieListScreen.js
@@ -1,27 +1,35 @@
 import React, { useState } from 'react';
 import { View } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
 import { SearchBar } from 'react-native-elements';
 import MovieList from '../components/MovieList';
+
+import { useDispatch, useSelector } from 'react-redux';
 import { searchActions } from "redux-logic-layer";
+import { useAppSelector, useAppDispatch } from '../App';
 
 const { userTyping, tvShowSelected } = searchActions;
 
 const MovieListScreen = ({ navigation }) => {
 
-  const [term, setTerm] = useState('');
   const movies = useSelector(({ search: { shows } }) => shows);
   const dispatcher = useDispatch();
+
+  const appDispatcher = useAppDispatch();
+  const count = useAppSelector(({ count }) => count);
+  console.log('count = ', count);
 
   const handleSearchTermChange = (query) => {
     setTerm(query);
     dispatcher(userTyping({ query }));
+    appDispatcher({ type: 'type1' });
   };
 
   const handleOnItemPress = async (item) => {
     dispatcher(tvShowSelected({ id: item.id }));
     navigation.navigate('MovieDetails', { title: item.name });
   }
+
+  const [term, setTerm] = useState('');
 
   return (
     <View style={{ flex: 1, backgroundColor: 'black' }}>
